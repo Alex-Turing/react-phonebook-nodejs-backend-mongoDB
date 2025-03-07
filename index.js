985c38feb4e0eb850bdfa332d9bdb9b91cc62709
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-03-07 12:55:16 -0500

    new commit

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const requestLogger = (request, response, next) => {
 morgan.token('body', (request) => JSON.stringify(request.body));
 
 app.use(express.json());
-app.use(express.static('dist'));
 app.use(cors());
+app.use(express.static('dist'));
 app.use(morgan('tiny'));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms - Body: :body'));
 app.use(requestLogger);
