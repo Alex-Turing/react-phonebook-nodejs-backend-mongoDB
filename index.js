1b7a96c05dbb1ac839a4d26b5e7d56652308b4a8
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-03-06 22:03:48 -0500

    dist folder added

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
-        "number": "040-123456"
+        "phoneNumber": "040-123456"
     },
     {
         "id": 2,
         "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        "phoneNumber": "39-44-5323523"
     },
     {
         "id": 3,
         "name": "Dan Abramov",
-        "number": "12-43-234345"
+        "phoneNumber": "12-43-234345"
     },
     {
         "id": 4,
         "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        "phoneNumber": "39-23-6423122"
     }
 ]
 
@@ -43,6 +43,7 @@ const requestLogger = (request, response, next) => {
 morgan.token('body', (request) => JSON.stringify(request.body));
 
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms - Body: :body'));
@@ -86,7 +87,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
-    if (!body.name || !body.number) {
+    if (!body.name || !body.phoneNumber) {
         return response.status(400).json({
             error: 'Name and number are required',
             status: 400,
@@ -106,7 +107,7 @@ app.post('/api/persons', (request, response) => {
     const newPerson = {
         id: generateId(),
         name: body.name,
-        number: body.number,
+        number: body.phoneNumber,
     };
 
     persons = persons.concat(newPerson);
@@ -115,7 +116,9 @@ app.post('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id',(request, response) =>{
     const personId = Number(request.params.id);
+    console.log('person ID:',personId);
     const actualPerson = persons.find(person => person.id === personId);
+    console.log('actualPerson:',actualPerson);
     if(!actualPerson) {
         return response.status(404).json({
             error: `Person with id ${personId} not found`,
