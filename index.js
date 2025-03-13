b4aace7513c56af56cd18ea44f3847fe1baff439
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-03-13 19:16:55 -0400

    name and number validation fixed in the front end

--- index.js
+++ index.js
@@ -137,10 +137,10 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message);
-    if (error.message === 'CastError') {
-        return response.status(400).send({ error: 'malformated id' });
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
     }
-    else if(error.message === 'ValidationError') 
+    else if(error.name === 'ValidationError') 
     {
         return response.status(404).json({ error: error.message });
     }
